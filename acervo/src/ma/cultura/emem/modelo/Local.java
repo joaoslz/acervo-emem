b0e9529313b85343c1f22cd5560df318d6b4295d
JoaoCarlos <JoaoCarlos@joaoPC>
2014-04-01 22:33:32 -0300

    Foi implementada a interface java.io.Serializable

--- acervo/src/ma/cultura/emem/modelo/Local.java
+++ acervo/src/ma/cultura/emem/modelo/Local.java
@@ -4,9 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Local {
+public class Local implements java.io.Serializable {
 
-    @Id
+	private static final long serialVersionUID = -2155196826342300173L;
+	@Id
     private String nome;
 
     public String getNome() {
