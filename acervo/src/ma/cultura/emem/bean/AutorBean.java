cd43ef4472e18327d76ee8c266b4a06a26e9ff6f
thiago <thiago@notenasper>
2014-04-12 16:10:42 -0300

    implementação da edição na tela de cadastro de autores.

--- acervo/src/ma/cultura/emem/bean/AutorBean.java
+++ acervo/src/ma/cultura/emem/bean/AutorBean.java
@@ -1,38 +1,60 @@
 package ma.cultura.emem.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.event.ActionEvent;
+import javax.faces.bean.ViewScoped;
 
 import ma.cultura.emem.dao.AutorDAO;
-import ma.cultura.emem.dao.DAO;
 import ma.cultura.emem.modelo.Autor;
 
-import org.primefaces.context.RequestContext;
+import org.primefaces.event.RowEditEvent;
 
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class AutorBean implements Serializable {
 
 	private static final long serialVersionUID = 3905906075866017417L;
 
+	private AutorDAO autorDAO = new AutorDAO();
 	private Autor autor = new Autor();
+	private List<Autor> autores = new ArrayList<Autor>();
+
+    /**
+     * Método para editar o autor direto da tabela.
+     * @param event
+     */
+	public void editAutor(RowEditEvent event) {  
+		Autor a = (Autor) event.getObject();
+		autorDAO.atualiza(a);
+	}  
+
+	public void gravar() {
+		autorDAO.adicionar(autor);
+//		Após o cadastro o autor é adicionado direto no ArrayList para evitar atualizar a lista com outro select
+		autores.add(0, autor);
+		autor = new Autor();
+	}
+
+	public void updateListaAutores() {
+		autores = autorDAO.listarAutoresPorIdEmOrdemDec();
+	}
+
+	public List<Autor> getAutores() {
+//		se a lista está vazia é porque o bean acabou de ser criado (nova view), 
+//		então faz uma consulta no banco para atualizar a lista.
+		if(autores.isEmpty())
+			updateListaAutores();
+		return autores;
+	}
 
 	public Autor getAutor() {
 		return autor;
 	}
-
-	public void gravar() {
-		new DAO<Autor>(Autor.class).adicionar(this.autor);
-		this.autor = new Autor();
+	
+	public void setAutor(Autor autor) {
+		this.autor = autor;
 	}
-
-
-	public List<Autor> getListaAutoresPorIdEmOrdemDec() {
-		return new AutorDAO().listarAutoresPorIdEmOrdemDec();
-	}
-
 }
