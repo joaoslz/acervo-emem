d112b98c7dcac52ee28faf4d57a4550a9d9dfa47
JoaoCarlos <JoaoCarlos@joaoPC>
2014-05-16 13:14:17 -0300

    Foram adicionadas marcações para as classes Metodo, Partitura, Idioma, Local e nova Classe Fasciculo... Um periodico por ter muitos fasciculos.

--- acervo/src/ma/cultura/emem/modelo/Fasciculo.java
+++ acervo/src/ma/cultura/emem/modelo/Fasciculo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,7 +14,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Fasciculo implements Serializable{
+public class Fasciculo implements Serializable {
 
 	private static final long serialVersionUID = -1467686486369558017L;
 
@@ -29,12 +30,18 @@ public class Fasciculo implements Serializable{
     private short paginaInicial;
     private short paginaFinal;
     
-    @ManyToOne
     private String mesInicio;
     private String mesFim;
     
+    @ManyToOne
+    private Periodico periodico;
+    
 	@OneToMany(mappedBy = "fasciculo")
 	private List<Exemplar> exemplares;
+		
+	
+	@OneToMany(mappedBy = "fasciculo", cascade=CascadeType.ALL, orphanRemoval = true)
+	private List<Artigo> artigos = new ArrayList<Artigo>();
 
     public int getId() {
         return id;
@@ -42,22 +49,6 @@ public class Fasciculo implements Serializable{
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAssunto() {
-        return assunto;
-    }
-
-    public void setAssunto(String assunto) {
-        this.assunto = assunto;
     }
 
     public short getPaginaInicial() {
@@ -76,12 +67,34 @@ public class Fasciculo implements Serializable{
         this.paginaFinal = paginaFinal;
     }
 
-    public Periodico getPeriodico() {
-        return periodico;
-    }
+    
+    
+	public String getArtigosToString() {
+		StringBuilder builder = new StringBuilder();
+		if (getArtigos() != null && !getArtigos().isEmpty()) {
+			Iterator<Artigo> it = getArtigos().iterator();
+			while (it.hasNext()) {
+				Artigo a = it.next();
+				builder.append(a.getTitulo());
+				if (it.hasNext()) {
+					builder.append(", ");
+				}
+			}
+		}
+		return builder.toString();
+	}
+    
+	public List<Artigo> getArtigos() {
+		return artigos;
+	}
 
-    public void setPeriodico(Periodico periodico) {
-        this.periodico = periodico;
-    }
+	public void setArtigos(List<Artigo> artigos) {
+		this.artigos = artigos;
+	}
+
+	public void adicionaArtigo(Artigo artigo) {
+		artigos.add(artigo);
+		artigo.setFasciculo(this);
+	}
 
 }
