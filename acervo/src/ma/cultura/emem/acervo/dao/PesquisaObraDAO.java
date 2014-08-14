d3809677c4ed21a0015d3d45f1ce4f9a1485b9b1
thiagonasper <thiagonasper@gmail.com>
2014-08-14 17:53:29 -0300

    Melhorias na tela de busca de obras.

--- acervo/src/ma/cultura/emem/acervo/dao/PesquisaObraDAO.java
+++ acervo/src/ma/cultura/emem/acervo/dao/PesquisaObraDAO.java
@@ -10,8 +10,10 @@ import ma.cultura.emem.acervo.dao.filtro.ObraFilter;
 import ma.cultura.emem.acervo.modelo.Obra;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.CriteriaSpecification;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
@@ -22,29 +24,38 @@ public class PesquisaObraDAO implements Serializable {
 
 	@Inject
 	private EntityManager em;
+	@Inject
+	private Logger logger;
 
 	@SuppressWarnings("unchecked")
 	public List<Obra> filtradas(ObraFilter filtro) {
 		Session session = this.em.unwrap(Session.class);
 
-		Criteria criteria = session.createCriteria(Obra.class);
+		Criteria criteria = session.createCriteria(Obra.class, "obra");
 
 		if (StringUtils.isNotBlank(filtro.getTitulo())) {
-			// pesquisa em qualquer substring que aparece em qualquer parte do Título da Obra
-			criteria.add(Restrictions.ilike("titulo", filtro.getTitulo(), MatchMode.ANYWHERE));
+			// pesquisa em qualquer substring que aparece em qualquer parte do
+			// Título da Obra
+			criteria.add(Restrictions.ilike("titulo", filtro.getTitulo(),
+					MatchMode.ANYWHERE));
 		}
 
 		if (StringUtils.isNotBlank(filtro.getSubtitulo())) {
-			// pesquisa em qualquer substring que aparece em qualquer parte do subtítulo da Obra
-			criteria.add(Restrictions.ilike("subtitulo", filtro.getSubtitulo(), MatchMode.ANYWHERE));
+			// pesquisa em qualquer substring que aparece em qualquer parte do
+			// subtítulo da Obra
+			criteria.add(Restrictions.ilike("subtitulo", filtro.getSubtitulo(),
+					MatchMode.ANYWHERE));
 		}
 
-		if (filtro.getAnoInicio() != null) {
-			// id deve ser maior ou igual (ge = greater or equals) a filtro.anoInicio
+		if (filtro.getAnoInicio() != null && filtro.getAnoInicio() > 0) {
+			logger.debug(filtro.getAnoInicio());
+			// id deve ser maior ou igual (ge = greater or equals) a
+			// filtro.anoInicio
 			criteria.add(Restrictions.ge("ano", filtro.getAnoInicio()));
 		}
 
-		if (filtro.getAnoFim() != null) {
+		if (filtro.getAnoFim() != null && filtro.getAnoFim() > 0) {
+			logger.debug(filtro.getAnoFim());
 			// id deve ser menor ou igual (le = lower or equal) a filtro.anoFim
 			criteria.add(Restrictions.le("ano", filtro.getAnoFim()));
 		}
@@ -52,6 +63,22 @@ public class PesquisaObraDAO implements Serializable {
 		if (filtro.getTiposObra() != null && !(filtro.getTiposObra().isEmpty())) {
 			// adicionamos uma restrição "in", passando uma lista de tipoObra
 			criteria.add(Restrictions.in("tipoObra", filtro.getTiposObra()));
+		}
+
+		if (filtro.getEditora() != null && filtro.getEditora().getId() != null) {
+			criteria.add(Restrictions.eq("editora.id", filtro.getEditora()
+					.getId()));
+		}
+		if (filtro.getAutores() != null && filtro.getAutores().size() > 0) {
+			// http://stackoverflow.com/questions/17701147/hibernate-criteria-join-query-one-to-many
+			criteria.createAlias("autores", "a");
+			criteria.add(Restrictions.in("a.id", filtro.getAutores()));
+			criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
+		}
+		if (filtro.getAssuntos() != null && filtro.getAssuntos().size() > 0) {
+			criteria.createAlias("assuntos", "s");
+			criteria.add(Restrictions.in("s.id", filtro.getAssuntos()));
+			criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
 		}
 
 		return criteria.addOrder(Order.asc("titulo")).list();
--- acervo/src/ma/cultura/emem/acervo/dao/filtro/ObraFilter.java
+++ acervo/src/ma/cultura/emem/acervo/dao/filtro/ObraFilter.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import ma.cultura.emem.acervo.modelo.auxiliar.Autor;
 import ma.cultura.emem.acervo.modelo.auxiliar.Editora;
 import ma.cultura.emem.acervo.modelo.auxiliar.TipoObra;
 
@@ -12,11 +13,14 @@ public class ObraFilter implements Serializable {
 	private static final long serialVersionUID = -8518090812834708389L;
 
 	private String titulo;
+	private String isbn;
 	private String subtitulo;
 	private Short anoInicio;
 	private Short anoFim;
 
 	private Editora editora;
+	private List<Integer> autores = new ArrayList<Integer>();
+	private List<Integer> assuntos = new ArrayList<Integer>();
 	private List<TipoObra> tiposObra = new ArrayList<TipoObra>();
 
 	public String getTitulo() {
@@ -67,4 +71,27 @@ public class ObraFilter implements Serializable {
 		this.editora = editora;
 	}
 
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public List<Integer> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Integer> autores) {
+		this.autores = autores;
+	}
+
+	public List<Integer> getAssuntos() {
+		return assuntos;
+	}
+
+	public void setAssuntos(List<Integer> assuntos) {
+		this.assuntos = assuntos;
+	}
 }
