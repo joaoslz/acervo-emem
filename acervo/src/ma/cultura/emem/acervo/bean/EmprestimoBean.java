31c833f514b89abf22d60641bc80fb8afdc01426
Thiago Nasper <Thiago Nasper@notenasper>
2014-08-31 19:02:40 -0300

    implementação da tela de empréstimos e visualização de empréstimos.

--- acervo/src/ma/cultura/emem/acervo/bean/EmprestimoBean.java
+++ acervo/src/ma/cultura/emem/acervo/bean/EmprestimoBean.java
@@ -1,15 +1,21 @@
 package ma.cultura.emem.acervo.bean;
 
 import java.io.Serializable;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import ma.cultura.emem.acervo.bean.datamodel.BaseEntityLazyDataModel;
 import ma.cultura.emem.acervo.dao.DAO;
+import ma.cultura.emem.acervo.dao.EmprestimoDAO;
+import ma.cultura.emem.acervo.modelo.Emprestimo;
 import ma.cultura.emem.acervo.modelo.Exemplar;
 import ma.cultura.emem.acervo.modelo.Usuario;
+import ma.cultura.emem.acervo.util.jpa.Transactional;
 import ma.cultura.emem.acervo.util.jsf.FacesMessages;
 
 import org.apache.log4j.Logger;
@@ -25,14 +31,41 @@ public class EmprestimoBean implements Serializable {
 	@Inject
 	private FacesMessages facesMsg;
 	
+	private BaseEntityLazyDataModel<Emprestimo> lazyDataModel;
+	
 	@Inject
 	private DAO<Exemplar> exemplarDAO;
 	@Inject
 	private DAO<Usuario> usuarioDAO;
+	@Inject
+	private EmprestimoDAO emprestimoDAO;
 
-	private Exemplar exemplar;
-	private Usuario usuario;
+	private Emprestimo emprestimo = new  Emprestimo();
 	
+	@PostConstruct
+	public void init(){
+		lazyDataModel = new BaseEntityLazyDataModel<Emprestimo>(emprestimoDAO.getDao());
+	}
+	
+	@Transactional
+	public void efetuarEmprestimo(){
+		logger.debug(".."+emprestimo.getObservacao());
+		if(emprestimo.getExemplar().isDisponivel()){
+			emprestimo.setDataEmprestimo(GregorianCalendar.getInstance());
+			//		emprestimo.setDataPrevista(null);//TODO estabelecer a regra para data prevista
+			emprestimo.getExemplar().setDisponivel(false);
+			emprestimoDAO.adicionar(emprestimo);
+			exemplarDAO.atualizar(emprestimo.getExemplar());
+			facesMsg.infoGlobal("Empréstimo realizado com sucesso!");
+		}else{
+			Emprestimo emp = emprestimoDAO.findUltimoEmprestimoEmAberto(emprestimo.getExemplar());
+			emp.setDataDevolucao(GregorianCalendar.getInstance());
+			emp.getExemplar().setDisponivel(true);
+			emprestimoDAO.atualizar(emp);
+			facesMsg.infoGlobal("Devolução realizada com sucesso!");
+		}
+		emprestimo = new Emprestimo();
+	}	
 	
 	public List<Exemplar> findExemplar(String id){
 		return exemplarDAO.findById(id);
@@ -43,18 +76,30 @@ public class EmprestimoBean implements Serializable {
 	}
 	
 	public void verificarStatusEmprestimo(){
-		//TODO
+		if(!emprestimo.getExemplar().isDisponivel()){
+			Emprestimo emp = emprestimoDAO.findUltimoEmprestimoEmAberto(emprestimo.getExemplar());
+			String nomeUsuario = emp.getUsuario().getNome();
+			facesMsg.warnGlobal("O exemplar <" + emprestimo.getExemplar().toString() + "> já está emprestado para " + nomeUsuario + "!");
+		}
 	}
 
-	public Exemplar getExemplar() {
-		return exemplar;
+	public boolean isEfetuarEmprestimo(){
+		if(emprestimo.getExemplar() == null || emprestimo.getExemplar().isDisponivel())
+			return true;
+		else
+			return false;
 	}
 
-	public void setExemplar(Exemplar exemplar) {
-		this.exemplar = exemplar;
+	public Emprestimo getEmprestimo() {
+		return emprestimo;
 	}
 
-	public Usuario getUsuario() {
-		return usuario;
+
+	public void setEmprestimo(Emprestimo emprestimo) {
+		this.emprestimo = emprestimo;
+	}
+
+	public BaseEntityLazyDataModel<Emprestimo> getLazyDataModel() {
+		return lazyDataModel;
 	}
 }
---
+++ acervo/src/ma/cultura/emem/acervo/dao/EmprestimoDAO.java
@@ -0,0 +1,113 @@
+package ma.cultura.emem.acervo.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import ma.cultura.emem.acervo.modelo.Emprestimo;
+import ma.cultura.emem.acervo.modelo.Exemplar;
+
+import org.apache.log4j.Logger;
+
+public class EmprestimoDAO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Logger logger;
+
+	@Inject
+	private EntityManager em;
+
+	@Inject
+	private DAO<Emprestimo> dao;
+	
+	public Emprestimo findUltimoEmprestimoEmAberto(Exemplar ex){
+		String jpql = "from Emprestimo e where e.dataDevolucao is null and e.exemplar.id = :idExemplar order by e.id desc";
+		TypedQuery<Emprestimo> query = em.createQuery(jpql, Emprestimo.class);
+		query.setParameter("idExemplar", ex.getId());
+		return query.getResultList().get(0);
+	}
+
+	public Emprestimo adicionar(Emprestimo t) {
+		return dao.adicionar(t);
+	}
+
+	public void adicionarLote(List<Emprestimo> lote) {
+		dao.adicionarLote(lote);
+	}
+
+	public Emprestimo atualizar(Emprestimo t) {
+		return dao.atualizar(t);
+	}
+
+	public void remover(Emprestimo t) {
+		dao.remover(t);
+	}
+
+	public List<Emprestimo> findById(String id) {
+		return dao.findById(id);
+	}
+
+	public List<Emprestimo> findByNome(String nome) {
+		return dao.findByNome(nome);
+	}
+
+	public int hashCode() {
+		return dao.hashCode();
+	}
+
+	public List<Emprestimo> getByNome(String nome) {
+		return dao.getByNome(nome);
+	}
+
+	public List<Emprestimo> findByProperty(String property, Object value) {
+		return dao.findByProperty(property, value);
+	}
+
+	public PaginatedResult<Emprestimo> findByPropertyAndPaginate(String property, Object value, int firstResult, int maxResultsByPage) {
+		return dao.findByPropertyAndPaginate(property, value, firstResult, maxResultsByPage);
+	}
+
+	public List<Emprestimo> findAll() {
+		return dao.findAll();
+	}
+
+	public List<Emprestimo> findAll(int maxResult) {
+		return dao.findAll(maxResult);
+	}
+
+	public boolean equals(Object obj) {
+		return dao.equals(obj);
+	}
+
+	public PaginatedResult<Emprestimo> findAllAndPaginate(int firstResult, int maxResultsByPage) {
+		return dao.findAllAndPaginate(firstResult, maxResultsByPage);
+	}
+
+	public PaginatedResult<Emprestimo> findFilteredAndPaginate(Map<String, Object> filters, int firstResult, int maxResultsByPage) {
+		return dao.findFilteredAndPaginate(filters, firstResult, maxResultsByPage);
+	}
+
+	public Emprestimo buscarPorId(Object id) {
+		return dao.buscarPorId(id);
+	}
+
+	public int contarTodos() {
+		return dao.contarTodos();
+	}
+
+	public String toString() {
+		return dao.toString();
+	}
+
+	public DAO<Emprestimo> getDao() {
+		return dao;
+	}
+	
+	
+}
