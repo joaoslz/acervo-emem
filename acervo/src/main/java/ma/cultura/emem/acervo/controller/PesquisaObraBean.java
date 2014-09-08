dec2bbc03abbe5c765eac7da7a7da3aec5f15534
thiagonasper <thiagonasper@gmail.com>
2014-09-08 17:19:04 -0300

    correção dos autocompletes na tela de pesquisa.

--- acervo/src/main/java/ma/cultura/emem/acervo/controller/PesquisaObraBean.java
+++ acervo/src/main/java/ma/cultura/emem/acervo/controller/PesquisaObraBean.java
@@ -9,7 +9,11 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import ma.cultura.emem.acervo.model.Obra;
+import ma.cultura.emem.acervo.model.auxiliar.Assunto;
+import ma.cultura.emem.acervo.model.auxiliar.Autor;
+import ma.cultura.emem.acervo.model.auxiliar.Editora;
 import ma.cultura.emem.acervo.repository.dto.ObraFilter;
+import ma.cultura.emem.acervo.service.ItemAcervoService;
 import ma.cultura.emem.acervo.service.PesquisaService;
 
 @Named
@@ -20,6 +24,8 @@ public class PesquisaObraBean implements Serializable {
 	
 	@Inject
 	private PesquisaService service;
+	@Inject
+	private ItemAcervoService itemAcervoService;
 
 	private ObraFilter filtro = new ObraFilter();
 	private List<Obra> obrasFiltradas = new ArrayList<Obra>();
@@ -39,6 +45,16 @@ public class PesquisaObraBean implements Serializable {
 	public void pesquisar() {
 		obrasFiltradas = service.filtrarObras(filtro);	
 	}
-	
-	
+
+	public List<Editora> findEditoras(String nome) {
+		return itemAcervoService.findEditoras(nome);
+	}
+
+	public List<Assunto> findAssuntos(String nome) {
+		return itemAcervoService.findAssuntos(nome);
+	}
+
+	public List<Autor> findAutores(String nome) {
+		return itemAcervoService.findAutores(nome);
+	}
 }
