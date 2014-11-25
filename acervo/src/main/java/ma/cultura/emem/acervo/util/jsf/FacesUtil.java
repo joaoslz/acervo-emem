f5402bc71827076fdf2ab79637e02697e2797cbc
JoaoCarlos <JoaoCarlos@joaoPC>
2014-11-24 22:58:23 -0300

    Implementação do Relatório de Etiquetas

--- acervo/src/main/java/ma/cultura/emem/acervo/util/jsf/FacesUtil.java
+++ acervo/src/main/java/ma/cultura/emem/acervo/util/jsf/FacesUtil.java
@@ -1,10 +1,15 @@
 package ma.cultura.emem.acervo.util.jsf;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
 
 public class FacesUtil {
+	
+	public static void addErrorMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+	}
 	
 	public static <T> T getBean(String name, Class<T> classe) {
 		FacesContext ctx = FacesContext.getCurrentInstance();
